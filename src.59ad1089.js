2f2f1f33c566dd30ae1a66101101877fbab58f1a
AsherWilens <ashwilens@gmail.com>
2022-10-31 11:46:13 -0400

    Update src.59ad1089.js

--- src.59ad1089.js
+++ src.59ad1089.js
@@ -72077,7 +72077,7 @@ function setupViewer() {
           _a.sent();
           // This must be called once after all plugins are added.
           viewer.renderer.refreshPipeline();
-          return [4 /*yield*/, manager.addFromPath("./assets/solitaires.glb")
+          return [4 /*yield*/, manager.addFromPath("./assets/10ctemscene.glb")
           // Load an environment map if not set in the glb file
           // await viewer.scene.setEnvironment(
           //     await manager.importer!.importSinglePath<ITexture>(
