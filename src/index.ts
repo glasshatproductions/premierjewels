6acd22707b0b61b1864289a6288a7eefd0ffb6dc
AsherWilens <ashwilens@gmail.com>
2022-10-31 12:06:37 -0400

    10ctanim

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/10ctemscene.glb")
+    await manager.addFromPath("./assets/10ctanim.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
