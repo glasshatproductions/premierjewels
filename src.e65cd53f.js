73eb6eafae1a6a20396e4332f5c11c45edc48b47
AsherWilens <ashwilens@gmail.com>
2022-10-31 16:27:08 -0400

    emscene

--- src.e65cd53f.js
+++ src.e65cd53f.js
@@ -72077,7 +72077,7 @@ function setupViewer() {
           _a.sent();
           // This must be called once after all plugins are added.
           viewer.renderer.refreshPipeline();
-          return [4 /*yield*/, manager.addFromPath("./assets/10ctblackem.glb")
+          return [4 /*yield*/, manager.addFromPath("./assets/emscene.glb")
           // Load an environment map if not set in the glb file
           // await viewer.scene.setEnvironment(
           //     await manager.importer!.importSinglePath<ITexture>(
