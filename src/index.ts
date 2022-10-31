136f7f45b3a6f6fe15922a78b08e5aef6eaa7a28
AsherWilens <ashwilens@gmail.com>
2022-10-31 17:01:59 -0400

    em

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/emhdr2.glb")
+    await manager.addFromPath("./assets/em.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
