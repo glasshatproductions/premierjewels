17d0a7a54fd962cf3caaf88658048fc221749468
AsherWilens <ashwilens@gmail.com>
2022-11-02 12:47:45 -0400

    em brace animated

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/bracelet+em+scene.glb")
+    await manager.addFromPath("./assets/br+em+anim.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
