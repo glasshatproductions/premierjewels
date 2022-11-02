f53103501a293e393eb56b27a6a3d6f7a3d3c247
glasshatproductions <ashwilens@gmail.com>
2022-11-02 10:15:25 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/em.glb")
+    await manager.addFromPath("./assets/solitaires.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
