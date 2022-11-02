94231ee7e5f441a473213eb80ef64d9fb2e66f4c
AsherWilens <ashwilens@gmail.com>
2022-11-02 13:43:52 -0400

    set1

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/premiergemset1.glb")
+    await manager.addFromPath("./assets/set1.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
